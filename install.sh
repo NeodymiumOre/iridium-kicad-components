8f1a2fb4fc0d250532d10d516ddcfd689e834ccf
NeodymiumOre <maciejbarna@gmail.com>
2024-02-20 10:48:49 +0100

    Updated script to install in XDG_DATA_HOME using fish

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
-#!/usr/bin/env sh
+#!/usr/bin/env fish
 echo "---------------------------"
 echo "Installing KiCad components"
 echo "---------------------------"
 
-echo "Symlink iridium library into ~/.local/share directory"
-mkdir --parents "${XDG_DATA_HOME:-$HOME/.local/share}"
-ln -sf $(pwd) "${XDG_DATA_HOME:-$HOME/.local/share/iridium-kicad-components}"
+set REPO iridium-kicad-components
 
-echo "----"
-echo "DONE"
-echo "----"
+# cmd for fish
+set -Ux XDG_DATA_HOME ~/.local/share
+
+echo "Symlink iridium library into $XDG_DATA_HOME directory"
+mkdir --parents "$XDG_DATA_HOME/$REPO"
+ln -sfn $(pwd)/symbols "$XDG_DATA_HOME/$REPO/symbols"
+ln -sfn $(pwd)/footprints "$XDG_DATA_HOME/$REPO/footprints.pretty"
+ln -sfn $(pwd)/3d-models "$XDG_DATA_HOME/$REPO/3d-models"
+echo "Done"
+
+echo "Don't forget to import libraries manually in KiCad!"
+
 
